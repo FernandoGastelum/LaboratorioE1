c8e083afb91f0a391718c96e4d4bd5acafa9e8e0
FernandoGastelum <144644818+FernandoGastelum@users.noreply.github.com>
2025-02-14 16:23:36 -0700

    Add files via upload

---
+++ LaboratorioE1/src/main/java/com/mycompany/laboratorioe1/LaboratorioE1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorioe1;
+
+/**
+ *
+ * @author gaspa
+ */
+public class LaboratorioE1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

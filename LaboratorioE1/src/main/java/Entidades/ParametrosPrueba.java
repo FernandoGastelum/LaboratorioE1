c9146f1a4f0d12b255b1678ed251ab4b56f33492
Ángel <86630479+Knocmare@users.noreply.github.com>
2025-02-22 16:31:53 -0700

    Add files via upload

--- LaboratorioE1/src/main/java/Entidades/ParametrosPrueba.java
+++ LaboratorioE1/src/main/java/Entidades/ParametrosPrueba.java
@@ -5,9 +5,49 @@
 package Entidades;
 
 /**
+ * Todos los elementos de los parametros de las pruebas.
  *
- * @author gaspa
+ * @author Ángel Ruíz García - 00000248171
  */
 public class ParametrosPrueba {
-    
+
+    private int id;
+    private int idPrueba;
+    private String nombreParametro;
+
+    public ParametrosPrueba(int id, int idPrueba, String nombreParametro) {
+        this.id = id;
+        this.idPrueba = idPrueba;
+        this.nombreParametro = nombreParametro;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdPrueba() {
+        return idPrueba;
+    }
+
+    public void setIdPrueba(int idPrueba) {
+        this.idPrueba = idPrueba;
+    }
+
+    public String getNombreParametro() {
+        return nombreParametro;
+    }
+
+    public void setNombreParametro(String nombreParametro) {
+        this.nombreParametro = nombreParametro;
+    }
+
+    @Override
+    public String toString() {
+        return "--- Parametro de Prueba ---\nID: " + id + "\nID de Prueba" + idPrueba + "\nParametro: " + nombreParametro;
+    }
+
 }
--- LaboratorioE1/src/main/java/Entidades/PruebaLaboratorio.java
+++ LaboratorioE1/src/main/java/Entidades/PruebaLaboratorio.java
@@ -5,9 +5,47 @@
 package Entidades;
 
 /**
+ * Todos los elementos de las pruebas de laboratorio.
  *
- * @author gaspa
+ * @author Ángel Ruíz García - 00000248171
  */
 public class PruebaLaboratorio {
+
+    private int id;
+    private String nombrePrueba;
+    private String categoria;
+
+    public PruebaLaboratorio(int id, String nombrePrueba, String categoria) {
+        this.id = id;
+        this.nombrePrueba = nombrePrueba;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombrePrueba() {
+        return nombrePrueba;
+    }
+    public void setNombrePrueba(String nombrePrueba) {
+        this.nombrePrueba = nombrePrueba;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "--- Prueba de Laboratorio ---\nID: " + id + "\nPrueba: " + nombrePrueba + "\nCategoria: " + categoria;
+    }
+    
     
 }

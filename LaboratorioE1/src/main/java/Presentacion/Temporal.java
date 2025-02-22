0bf638328fa1c910b4dd31843f5d5a5cd3a84fe6
FernandoGastelum <ilian.gastelum228761@potros.itson.edu.mx>
2025-02-21 21:39:26 -0700

    Paquetes y clases

---
+++ LaboratorioE1/src/main/java/Presentacion/Temporal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Presentacion;
+
+/**
+ *
+ * @author gaspa
+ */
+public class Temporal {
+    public static void main(String[] args) {
+        
+    }
+}

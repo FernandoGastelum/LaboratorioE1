0221b049099bafe75dff9cbc9bd301265e3a10d6
Paula A. García Galicia <133524087+paulaa55@users.noreply.github.com>
2025-02-23 19:06:49 -0700

    clase ClienteNegocio

--- LaboratorioE1/src/main/java/Negocio/ClienteNegocio.java
+++ LaboratorioE1/src/main/java/Negocio/ClienteNegocio.java
@@ -3,11 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Negocio;
+import DTOS.ClientesDTO;
+import Entidades.Cliente;
+
 
 /**
  *
- * @author gaspa
+ * @author gaspa,pau
  */
 public class ClienteNegocio {
-    
+    /**
+     * Convertir un objeto Cliente a un ClientesDTO
+     * @param cliente Objeto Cliente obtenido de la base de datos
+     * @return ClientesDTO info transformada
+     */
+    public ClientesDTO convertirAClientesDTO(Cliente cliente) {
+        if (cliente == null) {
+            return null;
+        }
+        String nombreCompleto = cliente.getNombre() + " " 
+                                  + cliente.getApellidoPaterno() + " " 
+                                  + cliente.getApellidoMaterno();
+        return new ClientesDTO(cliente.getId(), nombreCompleto, cliente.getFechaRegistro(), cliente.getFechaNacimiento());
+    }
 }

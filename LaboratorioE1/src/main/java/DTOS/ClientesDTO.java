1d1018c59cd6d58efa306e7535ec5d5d4f8a14ec
Paula A. García Galicia <133524087+paulaa55@users.noreply.github.com>
2025-02-23 18:33:39 -0700

    Clase DTO clientes
    todo lo referente a DTO

--- LaboratorioE1/src/main/java/DTOS/ClientesDTO.java
+++ LaboratorioE1/src/main/java/DTOS/ClientesDTO.java
@@ -4,10 +4,71 @@
  */
 package DTOS;
 
+import java.util.Date;
+
 /**
  *
- * @author gaspa
+ * @author gaspa,pau
  */
 public class ClientesDTO {
+    
+    private int id;
+    private String nombreCompleto;
+    private Date fechaRegistro;
+    private Date fechaNacimiento;
+    
+    
+    public ClientesDTO( ){
+}
+    //constructor con parametros
+    public ClientesDTO(int id, String nombreCompleto, Date fechaRegistro, Date fechaNacimiento) {
+        this.id = id;
+        this.nombreCompleto = nombreCompleto;
+        this.fechaRegistro = fechaRegistro;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    @Override
+    public String toString() {
+        return  "ClientesDTO " +
+                "idClienteDTO: " + id +
+                ", nombreCompleto: '" + nombreCompleto + '\'' +
+                ", fechaRegistro: " + fechaRegistro +
+                ", fechaNacimiento: " + fechaNacimiento +
+                '}';
+    }
+    
     
 }
